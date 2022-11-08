d47a9b82a684a4b2e5a964edaed9944b4e6cd18f
Kateryna Solodilova <kiivikat@gmail.com>
2022-11-08 19:31:37 +0200

    finishes task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function onPlay(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
